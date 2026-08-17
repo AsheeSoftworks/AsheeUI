1bc52a3e64eeeaa00c39409d8f66a4089d868fa7
AsheeSoftworks <asheesoftworks@gmail.com>
2026-08-17 17:41:00 +0000

    Remove @ashee/settings package

--- packages/settings/src/index.ts
+++
@@ -1,8 +0,0 @@
-export { useSettings } from "./react/useSettings";
-export { settingsController } from "./store";
-export type {
-  DensityLevel,
-  ExternalUserSettings,
-  UserSettings,
-} from "./types";
-export { defaultUserSettings } from "./types";
